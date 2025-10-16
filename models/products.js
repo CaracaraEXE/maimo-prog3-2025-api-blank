70cf0fcabd26279204d36e676418d373a9077d52
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-10-16 20:29:58 -0300

    added price tag

--- models/products.js
+++ models/products.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   name: { type: String },
   img:{type: String},
+  price:{type: Number},
   categories: [{type:Schema.Types.ObjectId,ref:"Category"}]
 });
 
